8294f88789926e436bbfd75d6227a2276495faab
Keyi Zhong <kz25@uw.edu>
2019-11-23 00:21:39 -0800

    add location check

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,62 +2,146 @@
 //获取应用实例
 const app = getApp()
 
+const odeLatitude = 47.656396;
+const odeLong = -122.310363;
 var query = wx.createSelectorQuery();
 
-Page({
-  data: {
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  bindFormSubmit:function(e){
-    wx.request({
-      url: "https://script.google.com/macros/s/AKfycbxX2XKX1Ajar6kSje2rmfgpIOpjH_na9d3_Yrm4h_PHF2Dj0RU/exec",
-      method: "post",
-      data: {
-        action: "checkIn",
-        name: e.detail.value.name,
-        email: e.detail.value.email
-      },
-      header:{
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      success: function (res) {
+function checkIn(e) {
+  return {
+    url: "https://script.google.com/macros/s/AKfycbxX2XKX1Ajar6kSje2rmfgpIOpjH_na9d3_Yrm4h_PHF2Dj0RU/exec",
+    method: "post",
+    data: {
+      action: "checkIn",
+      name: e.detail.value.name,
+      email: e.detail.value.email
+    },
+    header: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    success: function (res) {
+      if(res.data != "Success") {
+        if (res.data.length < 100) {
+          wx.showModal({
+            title: '出错',
+            content: res.data,
+          })
+        } else {
+          wx.showModal({
+            title: '出错',
+            content: 'oops，出错了，请联系管理员',
+          })
+        }
+      }else{
         wx.showToast({
-          title: '成功！',
+          title: 'Checked In',
           icon: 'success',
-          duration: 2000
-        })
-      }, fail: function (res) {
-        wx.showToast({
-          title: '出错',
-          icon: 'fail',
           duration: 2000
         })
       }
-    })
-  },
-  bindFormSubmit2: function (e) {
-    wx.request({
-      url: "https://script.google.com/macros/s/AKfycbxX2XKX1Ajar6kSje2rmfgpIOpjH_na9d3_Yrm4h_PHF2Dj0RU/exec",
+    }, fail: function (res) {
+      wx.showToast({
+        title: '出错',
+        icon: 'fail',
+        duration: 2000
+      })
+    }
+  }
+}
+
+
+function checkOut(e) {
+  return {
+    url: "https://script.google.com/macros/s/AKfycbxX2XKX1Ajar6kSje2rmfgpIOpjH_na9d3_Yrm4h_PHF2Dj0RU/exec",
       method: "POST",
-      data: {
-        action: "checkOut",
+        data: {
+      action: "checkOut",
         name: e.detail.value.name
-      },
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },  
-      success: function (res) {
-        console.log(res.data);
+    },
+    header: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    success: function (res) {
+      if (res.data != "Success") {
+        if (res.data.length < 100){
+          wx.showModal({
+            title: '出错',
+            content: res.data,
+          })
+        } else {
+          wx.showModal({
+            title: '出错',
+            content: 'oops，出错了，请联系管理员',
+          })
+        }
+      } else {
         wx.showToast({
-          title: '成功！',
+          title: 'Checked out',
           icon: 'success',
           duration: 2000
         })
-      },
-    })
+      }
+    }, fail: function (res) {
+      wx.showToast({
+        title: '出错',
+        duration: 2000
+      })
+    }
+  }
+}
+
+function checkLocation() {
+  var isODE = false;
+  var tolerance = 0.001;
+  wx.getLocation({
+    success: function (res) {
+      var latitude = res.latitude
+      var longitude = res.longitude
+      if(odeLatitude > latitude - tolerance && odeLatitude < latitude + tolerance && odeLong > longitude - tolerance && odeLong < longitude + tolerance) {
+        isOde = true;
+      }
+    },
+    altitude: false
+  })
+  return isODE;
+}
+
+Page({
+  data: {
+    userInfo: "",
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    title:"UWCTA 自习打卡",
+    description: "请注意这是测试版，check in 和check out的时候名字必须完全match，邮箱为optional 可以填也可以不填，之后源表格就会变成只读状态，打卡只可以从这个小程序进行"
+  },
+  bindFormSubmit:function(e){
+    if(checkLocation()) {
+      wx.request(checkIn(e));
+    }else {
+      wx.showModal({
+        title: '提示',
+        content: '您目前不在ODE',
+        success: function (res) {
+        }
+      })
+    }
+  },
+  bindFormSubmit2: function (e) {
+    if (checkLocation()) {
+      wx.request(checkOut(e));
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '您目前不在ODE',
+        success: function (res) {
+        }
+      })
+    }
   },
   onLoad: function () {
+    wx.showToast({
+      title: 'loading',
+      icon:'loading',
+    })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
